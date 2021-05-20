e69641682b6c7ea22f5a5a51f49a7894739084f0
Mahesh Gaikwad <maheshgaikwad8892@gmail.com>
2021-05-20 14:31:11 +0530

    added axios and api

--- src/views/Slots.vue
+++ src/views/Slots.vue
@@ -5,8 +5,23 @@
 </template>
 
 <script>
+
+import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+Vue.use(VueAxios, axios)
+
 export default {
     name:'Slots',
+    methods:{
+        getData(){
+            this.axios.get('https://cdn-api.co-vin.in/api/v2/admin/location/states')
+            .then((data) => console.log(data.data))
+        }
+    },
+    mounted(){
+        this.getData()
+    }
     
 }
 </script>
